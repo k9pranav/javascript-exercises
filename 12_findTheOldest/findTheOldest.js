2ae4e773e9b6b1286e1a358b9e3f75c6d2b63957
k9pranav <k9.pranav@gmail.com>
2023-06-03 17:42:44 -0400

    completed findOldest

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,6 +1,23 @@
-const findTheOldest = function() {
+var currentYear = new Date().getFullYear()
 
-};
+function getAge(arrayComp){
+    if (!arrayComp.yearOfDeath){
+        return currentYear - arrayComp.yearOfBirth
+    } else{
+        return arrayComp.yearOfDeath - arrayComp.yearOfBirth
+    }
+}
+
+const findTheOldest = function(array) {
+    const sorted = array.sort(function(a, b) {
+        const lastAge = getAge(a)
+        const currentAge = getAge(b)
+        return lastAge > currentAge ? -1 : 1
+    });
+
+    return sorted[0]
+}
+
 
 // Do not edit below this line
 module.exports = findTheOldest;
